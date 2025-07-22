5c7d25b4f1a6b0ef3236511519570201408c3bba
ohaddahan <ohaddahan@gmail.com>
2025-07-22 15:12:52 +0300

    publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-
+export _PWD="$(pwd)"
+export ROOT="$(git rev-parse --show-toplevel)"
+for dir in "${ROOT}"/libs/* ; do
+    if [ -d "$dir" ]; then
+        cd "$dir" || exit 1
+        echo "Running command in $dir"
+        cargo publish
+        if [ $? -ne 0 ]; then
+            echo "Error in $dir, stopping"
+            exit 1
+        fi
+        cd "${ROOT}"
+    fi
+done
